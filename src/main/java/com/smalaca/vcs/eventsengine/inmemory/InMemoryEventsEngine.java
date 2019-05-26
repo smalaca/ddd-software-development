eeed847dbee70c2cdd52454ee488be634f63c38a
smalaca <ab13.krakow@gmail.com>
2019-05-26 17:22:19 +0200

    InMemoryEventsEngine should implement EventsEngine

--- src/main/java/com/smalaca/vcs/eventsengine/inmemory/InMemoryEventsEngine.java
+++ src/main/java/com/smalaca/vcs/eventsengine/inmemory/InMemoryEventsEngine.java
@@ -1,18 +1,21 @@
 package com.smalaca.vcs.eventsengine.inmemory;
 
 import com.smalaca.vcs.domain.commit.events.CommitAdded;
+import com.smalaca.vcs.domain.eventsengine.EventsEngine;
 import com.smalaca.vcs.domain.eventsengine.Subscriber;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class InMemoryEventsEngine {
+public class InMemoryEventsEngine implements EventsEngine {
     private final List<Subscriber> subscribers = new ArrayList<>();
 
+    @Override
     public void register(Subscriber subscriber) {
         subscribers.add(subscriber);
     }
 
+    @Override
     public void publish(CommitAdded commitAdded) {
         subscribers.forEach(subscriber -> subscriber.handle(commitAdded));
     }
