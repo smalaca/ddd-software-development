ac794aa5264d32334951be6e4c052e606cad5549
smalaca <ab13.krakow@gmail.com>
2019-05-28 20:33:05 +0200

    Add usage of BuildRepository into BuildTriggeredListener

--- src/main/java/com/smalaca/ci/application/build/BuildTriggeredListener.java
+++ src/main/java/com/smalaca/ci/application/build/BuildTriggeredListener.java
@@ -2,18 +2,23 @@ package com.smalaca.ci.application.build;
 
 import com.smalaca.ci.domain.build.Build;
 import com.smalaca.ci.domain.build.BuildFactory;
+import com.smalaca.ci.domain.build.BuildRepository;
 import com.smalaca.ci.domain.build.events.BuildTriggered;
 
 public class BuildTriggeredListener {
     private final BuildFactory buildFactory;
+    private final BuildRepository buildRepository;
 
-    public BuildTriggeredListener(BuildFactory buildFactory) {
+    public BuildTriggeredListener(BuildFactory buildFactory, BuildRepository buildRepository) {
         this.buildFactory = buildFactory;
+        this.buildRepository = buildRepository;
     }
 
     public void listen(BuildTriggered buildTriggered) {
         Build build = buildFactory.create(buildTriggered.pipelineId(), buildTriggered.buildType());
 
         build.start();
+
+        buildRepository.save(build);
     }
 }
--- src/main/java/com/smalaca/ci/domain/build/BuildRepository.java
+++ src/main/java/com/smalaca/ci/domain/build/BuildRepository.java
@@ -2,4 +2,6 @@ package com.smalaca.ci.domain.build;
 
 public interface BuildRepository {
     Build find(BuildId buildId);
+
+    void save(Build build);
 }
